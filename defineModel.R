7a47ee627fa2aa7219203de840f146d7672cb080
Cameron Kieffer <cameron.bobo02@gmail.com>
2018-02-20 13:14:03 -0600

    Deprecate model_1drug.R
    Combined model_1drug.R with model_2drugs.R. _2drugs should be able to
    model just about any model now with any combination of unknowns.

---
+++ defineModel.R
@@ -0,0 +1,45 @@
+#combine the different ODE defined models that can exist for the system
+
+#simple vs. complex ACH model
+#one NT vs two NTs vs more NTs(?)
+#summation method of model if more than two neurotransmitters are used
+
+#ODEinits for one drug vs two drugs
+defineModel <- function(ACH_mod="simple", unk_mod="none", effect_mod = "oneNT"){
+  ach_models <- list(
+    none = "",
+    simple = "d/dt(depot2) = -KA2*depot2;
+            d/dt(centr2) = KA2*depot2 - KE2*centr2;
+    ",
+    complex = "d/dt(depot2) = -KA2*depot2 + KA2*KA2f*depot2*depot2/(depot2 + IC501);
+             d/dt(centr2) = KA2*depot2 - KA2*KA2f*depot2*depot2/(depot2 + IC501) - KE2*centr2 + KE2*KE2f*centr2*centr2/(centr2 + IC502);
+    ")
+
+  unk_models <- list(
+    none = "",
+    simple = "d/dt(depot1) = -KA1*depot1;
+              d/dt(centr1) = KA1*depot1 - KE1*centr1;
+    "
+  )
+
+  effect_models <- list(
+    none = "",
+    oneNT = "eff2 = centr2/(EC502+centr2);",
+    twoNT1 = "effa = max1*centr1/(EC501 + centr1);
+            effb = centr2/(EC502 + centr2);
+            eff2 = effb - effa + (effa * effb);",
+    twoNT2 = "eff1 = max1*centr1/(EC501 + centr1);
+            eff2 = (1-eff1)*centr2/(EC502 + centr2);"
+  )
+  ODE1 <- paste(unk_models[unk_mod], ach_models[ACH_mod], effect_models[effect_mod])
+
+  mod1 <- RxODE(model = ODE1, modName = "mod1") #Compile Model
+
+  if(unk_mod == "none"){
+    ODEinits <- c(depot2 = 0, centr2 = 0) #initial conditions for single neurotransmitter
+  } else{
+    ODEinits <- c(depot1 = 0, centr1 = 0, depot2 = 0, centr2 = 0) #initial conditions for two neurotransmitters
+  }
+
+  return(list(mod1, ODEinits))
+}
--- model_2drugs.R
+++ model_2drugs.R
@@ -1,75 +1,36 @@
 #This script iteratively solves a system of ODEs and finds parameters
-#the model is for two neurotransmistters, one ACH, another unknown relaxant
+#the model is defined in "defineModel.R" It can work with a single ACH-like neurotransmitter (NT)
+#or it can use two neurotransmitters, one constrictor (acetylcholine; ACH) and one relaxant (unknown; unk)
 
 #RxODE
 library(RxODE)
 library(ggplot2)
 source("normalizedDF.R")
+source("defineModel.R")
 
-#choose a set of ODEs that describe the desired model
-prepareModel <- function(mod_num = 1){
-  if(mod_num == 1){
-    #statistical summation 2-drug model
-    ODE1 <- "
-    d/dt(depot1) = -KA1*depot1;
-    d/dt(centr1) = KA1*depot1 - KE1*centr1;
-
-    d/dt(depot2) = -KA2*depot2;
-    d/dt(centr2) = KA2*depot2 - KE2*centr2;
-
-    effa = max1*centr1/(EC501 + centr1);
-    effb = centr2/(EC502 + centr2);
-    eff2 = effb - effa + (effa * effb);
-    "
-  }  else if(mod_num == 2){
-    #decrease maximum 2-drug model
-    ODE1 <- "
-    d/dt(depot1) = -KA1*depot1;
-    d/dt(centr1) = KA1*depot1 - KE1*centr1;
-
-    d/dt(depot2) = -KA2*depot2;
-    d/dt(centr2) = KA2*depot2 - KE2*centr2;
-
-    eff1 = max1*centr1/(EC501 + centr1);
-    eff2 = (1-eff1)*centr2/(EC502 + centr2);
-    "
-  }
-
-  mod1 <- RxODE(model = ODE1, modName = "mod1") #Compile Model
-  ODEinits <- c(depot1 = 0, centr1 = 0, depot2 = 0, centr2 = 0)  #initial conditions
-
-  return(list(mod1, ODEinits))
-}
-thismodel <- prepareModel(1)
+thismodel <- defineModel(ACH_mod="simple", unk_mod="none", effect_mod = "oneNT")
 
 #a function that takes parameters and runs the model
-run_2drugs_mod1 <- function(stim_freq, final_mod_params, NT=2){
-  if (NT==2){
-    #(KAunk, KEunk, logDVunk, logEC50unk, MAXunk)
-    mod_params <- c(KA1 = final_mod_params[1],
-                    KE1 = final_mod_params[2],
-                    KA2 = KAach,
-                    KE2 = KEach,
-                    max1 = final_mod_params[5],
-                    EC501 = 10^-final_mod_params[4],
-                    EC502 = 10^-EC50ach
-    )
-    time_var <- 60 #length of stimulation in seconds
+run_mod1 <- function(NT=2, stim_freq, mod_params){
+  parameters <- c(KA1 = mod_params$KAunk,
+                  KE1 = mod_params$KEunk,
+                  KA2 = mod_params$KAach,
+                  KE2 = mod_params$KEach,
+                  max1 = mod_params$MAXunk,
+                  EC501 = 10^-mod_params$EC50unk,
+                  EC502 = 10^-mod_params$EC50ach
+              )
+  time_var <- 60 #length of stimulation in seconds
+
+   if (NT == 2){
     pulse_rate <- 1/stim_freq
     num_doses <- time_var/pulse_rate
     testev <- eventTable(amount.units="mol", time.unit="seconds")
-    testev$add.dosing(dose = 10^-final_mod_params[3], nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 1, start.time = 0)
-    testev$add.dosing(dose = 10^-DVach, nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 3, start.time = 0)
+    testev$add.dosing(dose = 10^-mod_params$DVunk, nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 1, start.time = 0)
+    testev$add.dosing(dose = 10^-mod_params$DVach, nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 3, start.time = 0)
     testev$add.sampling(seq(from = 0, to = time_var, by = 0.02))
+  } else if(NT == 1){
 
-    finalres <- thismodel[[1]]$run(mod_params, testev, thismodel[[2]])
-  } else if(NT==1){
-    mod_params <- c(KA2 = final_mod_params[1],
-                    KE2 = KEach1,
-                    KE3 = KEach2,
-                    EC502 = EC50ach
-    )
-    time_var <- 60 #length of stimulation in seconds
     if(stim_freq == 0.1){
       pulse_rate <- 1/stim_freq
     } else{
@@ -77,17 +38,17 @@ run_2drugs_mod1 <- function(stim_freq, final_mod_params, NT=2){
     }
     num_doses <- time_var/pulse_rate
     testev <- eventTable(amount.units="mol", time.unit="seconds")
-    testev$add.dosing(dose = 10^-DVach, nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 1, start.time = 0)
+    testev$add.dosing(dose = 10^-mod_params$DVach, nbr.doses = num_doses, dosing.interval = pulse_rate, dosing.to = 1, start.time = 0)
     testev$add.sampling(seq(from = 0, to = time_var, by = 0.02))
-
-    finalres <- mod1$run(mod_params, testev, ODEinits)
   }
+
+  finalres <- thismodel[[1]]$run(parameters, testev, thismodel[[2]])
   return(finalres)
 }
 
 #returns a dataframe of chosen/tested parameters from the model
-#conDF should be formatted using loadNormalizedDF (see below)
-final_2Dparams <-function(stim_freq, m = 50, conDF, init_params, init_model, NT=2, chosen=TRUE){
+#conDF should be formatted using loadNormalizedDF
+final_drug_params <-function(NT=2, stim_freq, m = 50, conDF, bestfit, init_params, init_model, chosen=TRUE){
 
   testexp <- 4     #sum of "squares" exponenet
   lambda <- 2    #learning rate
@@ -102,13 +63,13 @@ final_2Dparams <-function(stim_freq, m = 50, conDF, init_params, init_model, NT=
   tested_params <- init_params
 
   for (j in 1:m){
-    for(i in 1:length(init_params)){
+    for(i in bestfit){
       testparams <- newparams
-      testparams[i] <- abs(rnorm(n = 1, mean = newparams[i], sd = (sqrt(init_params[i]^2)*lambda)))
+      testparams[[i]] <- abs(rnorm(n = 1, mean = newparams[[i]], sd = (sqrt(init_params[[i]]^2)*lambda)))
       tested_params <- rbind(tested_params, testparams)
 
-      #calls run_2drugs_mod1 to run the model
-      iteration <- run_2drugs_mod1(stim_freq, testparams, NT=NT)
+      #calls run_mod1 to run the model
+      iteration <- run_mod1(NT=NT, stim_freq, testparams)
 
       #calculate Sum of Squares Objective function for the new model
       workingdata$newmodel <- iteration[1:length(workingdata$Model),"eff2"]
@@ -134,39 +95,34 @@ final_2Dparams <-function(stim_freq, m = 50, conDF, init_params, init_model, NT=
   }
 }
 
-#Define initial parameters - > ACh consensus values here
-##Decide on format here
-KAach <- 0.2417
-KEach <- 0.5268
-DVach <- 5.902
-EC50ach <- 5.383
-init_params_ach <- list(KAach = 0.2417,
-                        KEach = 0.5268,
-                        DVach = 5.902)
+#Define initial parameters.
+#for 2 NT: ACh consensus values here followed by initial unknown parameters
+init_params <- list(KAach = 0.2417,
+                    KEach = 0.5268,
+                    DVach = 5.902,
+                    EC50ach = 5.383,
 
-#Relaxation initial parameters
-KAunk <- 1
-KEunk <- 1
-DVunk <- 7
-EC50unk <- 5
-MAXunk <- 1
-init_params_unk <- list(KAunk = 1, KEunk = 1, DVunk = 7, EC50unk = 5, MAXunk = 1)
+                    KAunk = 1,
+                    KEunk = 1,
+                    DVunk = 7,
+                    EC50unk = 5,
+                    MAXunk = 1)
 
-#Unknown parameters in order (KAunk, KEunk, DVunk, EC50unk, MAXunk)
-init_params1 <- c(KAunk, KEunk, DVunk, EC50unk, MAXunk)
-freq_list <- c(0.1, 0.3, 0.7, 1, 3, 7, 10 , 15, 30)
+#Define what model we are running
+freq0 <- 1                               #what frequency
+num_NT <- 1                              #how many neurotransmitters (should be informed by the model used)
+bestfit <- c("KAach", "KEach", "DVach")  #what unknowns are being solving for?
 
-working_freq <- 30
-WconDF <- loadNormalizedDF(1, lower = FALSE, dataDF = "con", normDF = "con")
-initialresults <- run_2drugs_mod1(stim_freq = working_freq, init_params1)
-finalparams <- final_2Dparams(stim_freq = working_freq, m = 500, WconDF, init_params1, initialresults)
-finalresults <- run_2drugs_mod1(stim_freq = working_freq, finalparams[nrow(finalparams),])
+WconDF <- loadNormalizedDF(1, lower = TRUE, dataDF = "cap", normDF = "cap")
+initialresults <- run_mod1(NT = num_NT, stim_freq = freq0, init_params)
+finalparams <- final_drug_params(NT = num_NT, stim_freq = freq0, m = 500, WconDF, bestfit = bestfit, init_params, initialresults)
+finalresults <- run_mod1(NT = num_NT, stim_freq = freq0, finalparams[nrow(finalparams),])
 
 p30<- (ggplot() #plot
-      + geom_path(aes(x=WconDF$Time, y=WconDF[,paste0("X", working_freq, "HZ")]), color="black", alpha = 0.5)
+      + geom_path(aes(x=WconDF$Time, y=WconDF[,paste0("X", freq0, "HZ")]), color="black", alpha = 0.5)
       + geom_path(aes(x=initialresults[,"time"], y=initialresults[,"eff2"]), color="green",size = 1.5)
       + geom_path(aes(x=finalresults[,"time"], y=finalresults[,"eff2"]), color="red",size = 1.5)
-      +labs(list(title=paste0(working_freq, " HZ Upper Responses 2Drugs"), x="Time (s)", y='Constriction'))
+      +labs(list(title=paste0(freq0, " HZ Upper Responses 2Drugs"), x="Time (s)", y='Constriction'))
       +theme_bw()
       +xlim(0,75)
 )
@@ -177,13 +133,13 @@ massIteration <- function(con_list = c(1,2,5,7), dataDF = "con", normDF = "cap",
   for(r in con_list){                                                 #iterates through each raw file
     for (q in freq_list){                                             #iterates through each frequency
       WconDF <- loadNormalizedDF(r, lower = lower, dataDF = dataDF, normDF = normDF) #load file
-      initialresults <- run_2drugs_mod1(init_params1, q)              #find starting point model results from initial parameters
-      finalparams <- final_2Dparams(q, m=500, WconDF, init_params1, initialresults)
+      initialresults <- run_mod1(init_params1, q)              #find starting point model results from initial parameters
+      finalparams <- final_drug_params(q, m=500, WconDF, init_params1, initialresults)
 
       finalparamsDF <- c(q, finalparams[nrow(finalparams),])          #take initial parameters and start vector for final values
 
       for(k in seq(1:100)){
-        finalparams <- final_2Dparams(q, m=500, WconDF, init_params1, initialresults)
+        finalparams <- final_drug_params(q, m=500, WconDF, init_params1, initialresults)
         finalparamsDF <- rbind(finalparamsDF, c(q, finalparams[nrow(finalparams),]))
       }
 
@@ -194,25 +150,21 @@ massIteration <- function(con_list = c(1,2,5,7), dataDF = "con", normDF = "cap",
 }
 
 #produces graphable consensus data
+freq_list <- c(0.1, 0.3,0.7,1, 3, 7, 10, 15, 30)
 consensus_models <- initialresults[,'time']
 for (q in freq_list){
-  #ACh consensus values here
-  KAach <- 0.2417
-  KEach <- 0.5268
-  DVach <- 10^-5.902
-  EC50ach <- 10^-5.383
+  init_params <- list(KAach = 0.2417,
+                      KEach = 0.5268,
+                      DVach = 5.902,
+                      EC50ach = 5.383,
 
-  #Relaxation consensus values
-  KAunk <- 0.9661
-  KEunk <- 10.8071
-  DVunk <- 7.4434
-  EC50unk <- 4.048
-  MAXunk <- 0.861135
-  init_params1 <- c(KAunk, KEunk, DVunk, EC50unk, MAXunk)
+                      KAunk = 0.9661,
+                      KEunk = 10.8071,
+                      DVunk = 7.4434,
+                      EC50unk = 4.048,
+                      MAXunk = 0.861135)
 
-  initialresults <- run_2drugs_mod1(init_params1, q) #run model with consensus values
+  initialresults <- run_mod1(NT = 2, q, init_params) #run model with consensus values
   consensus_models <- cbind(consensus_models, initialresults[,'eff2']) #append freq results
-
 }
 write.csv(consensus_models, file = paste0(con_directory_name, "consensus_2drugs.csv"))
-
