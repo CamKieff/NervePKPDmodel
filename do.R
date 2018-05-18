fa58194b97a866e5207ddfecfe3ba7a0ca531ee4
Cameron Kieffer <cameron.bobo02@gmail.com>
2018-05-18 10:43:01 -0500

    Updated Gitignore and do

--- do.R
+++ do.R
@@ -100,20 +100,22 @@ aggregate_stats(con_list = c(1,2,5,7), input_fileform = "FormattedLowerTrachea/c
                 output_filename = "FormattedLowerTrachea/control/con_aggregate_complex.csv")
 
 #statistics for all frequnecy tissue specific numbers. Creates a single final parameter file.
-cap_simple <- 
-  read.csv("FormattedLowerTrachea/capsaicin/cap_aggregate_Hz.csv", header = TRUE) %>%
-  filter(var =="median") %>%
-  #plyr::rename(c("Group.1" = "Freq", "Group.2" = "Tissue")) %>% #aggregate_stats should do this
-  aggregate(by=list(.$Tissue), mean) %>%
-  mutate(Capsaicin = 1, Complex = 0) %>%
-  select(-Group.1, -X, -Freq, -var)
-
 con_simple <- 
-  read.csv("FormattedLowerTrachea/control/con_aggregate_Hz.csv", header = TRUE) %>%
+  freq_results <- rbind(read.csv("FormattedLowerTrachea/control/con_aggregate_0.1Hz.csv", header = TRUE), 
+                        read.csv("FormattedLowerTrachea/control/con_aggregate_Hz.csv")) %>%
   filter(var =="median") %>%
   #plyr::rename(c("Group.1" = "Freq", "Group.2" = "Tissue")) %>%
   aggregate(by=list(.$Tissue), FUN=mean) %>%
   mutate(Capsaicin = 0, Complex = 0) %>%
+  select(-Group.1, -X, -Freq, -var)
+
+cap_simple <- 
+  rbind(read.csv("FormattedLowerTrachea/capsaicin/cap_aggregate_0.1Hz.csv", header = TRUE), 
+        read.csv("FormattedLowerTrachea/capsaicin/cap_aggregate_Hz.csv")) %>%
+  filter(var =="median") %>%
+  #plyr::rename(c("Group.1" = "Freq", "Group.2" = "Tissue")) %>% #aggregate_stats should do this
+  aggregate(by=list(.$Tissue), mean) %>%
+  mutate(Capsaicin = 1, Complex = 0) %>%
   select(-Group.1, -X, -Freq, -var)
 
 cap_complex <- 
@@ -165,10 +167,10 @@ meanRes <- rbind(mutate(aggregate(freq_results, by=list(freq_results$Freq), mean
 write.csv(meanRes, "FormattedLowerTrachea/capsaicin/cap_frequency_complex.csv")
 
 #find frequency specific results - control - complex model
-freq_results <- read.csv("FormattedLowerTrachea/control/cap_aggregate_complex.csv") %>%
+freq_results <- read.csv("FormattedLowerTrachea/control/con_aggregate_complex.csv") %>%
   filter(var == "median") %>%
   select(Freq, m2max, chemax, IC50m2, IC50che) 
 
 meanRes <- rbind(mutate(aggregate(freq_results, by=list(freq_results$Freq), mean), var ="mean"),
                  mutate(aggregate(freq_results, by=list(freq_results$Freq), function(x) sd(x)/sqrt(4)), var ="SEM"))
-write.csv(meanRes, "FormattedLowerTrachea/capsaicin/cap_frequency_complex.csv")
+write.csv(meanRes, "FormattedLowerTrachea/control/con_frequency_complex.csv")
